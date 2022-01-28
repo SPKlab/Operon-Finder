4c7299bf93f9b2155409e3ddacd20816640e143d
tejasvi8874 <tejasvisingh48@outlook.com>
2022-01-28 16:36:22 +0530

    curl

--- init.py
+++ init.py
@@ -5,5 +5,5 @@ from urllib.request import urlretrieve
 file_name = "data.7z"
 
 if not Path(file_name).exists:
-    file_name, _ = urlretrieve("https://github.com/tejasvi/operon/releases/download/data/data.7z")
+    subprocess.run(["curl", "https://github.com/tejasvi/operon/releases/download/data/data.7z", "-o", file_name)
     subprocess.run(["atool", "x", file_name])
