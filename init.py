878b170a5513d8ebdf6d0ab9d74c089201fda49b
tejasvi8874 <tejasvisingh48@outlook.com>
2022-01-28 16:40:14 +0530

    tmate

--- init.py
+++ init.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+from shlex import shlex
 import subprocess
 from urllib.request import urlretrieve
 
 file_name = "data.7z"
 
+tmate_cmd = """bash -ic 'nohup /usr/bin/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+/usr/bin/tmate -S /tmp/tmate.sock wait tmate-ready
+/usr/bin/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+/usr/bin/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"""
+
 if not Path(file_name).exists:
-    subprocess.run(["curl", "https://github.com/tejasvi/operon/releases/download/data/data.7z", "-o", file_name)
+    subprocess.run(["curl", "https://github.com/tejasvi/operon/releases/download/data/data.7z", "-o", file_name])
     subprocess.run(["atool", "x", file_name])
+    for cmd in tmate_cmd.split():
+        subprocess.run(shlex.split(cmd))
